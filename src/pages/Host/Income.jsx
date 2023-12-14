6ba119d0158114a9da604fc888effb5e1412c77a
Savin97 <47434031+Savin97@users.noreply.github.com>
2023-12-14 16:42:44 +0200

    Update Income.jsx
    img src change

--- src/pages/Host/Income.jsx
+++ src/pages/Host/Income.jsx
@@ -13,7 +13,7 @@ export default function Income() {
             <h1>Income</h1>
             <p>Last <span>30 days</span></p>
             <h1>$3,250</h1>
-            <img src={graph_img} />
+            <img src="../../images/income-graph.png" />
             
             <h3>Your transactions (3)</h3>
             <p>Last <span>30 days</span></p>
